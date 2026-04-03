b1a4c63855a414bb24b66fd4c4775c351ed64e27
Nikita <n.shmykov@yandex.ru>
2026-04-03 13:20:19 +0600

    создана автогенерация карты с коридорами

--- World/map_manager.gd
+++ World/map_manager.gd
@@ -1,65 +1,54 @@
 extends Node2D
 
-@export var corridor_h_scene: PackedScene # Перетащи CorridorH сюда
-@export var corridor_v_scene: PackedScene
+# --- НАСТРОЙКИ ---
 @export var room_scene: PackedScene 
+@export var corridor_h_scene: PackedScene # Перетащи CorridorH сюда
+@export var corridor_v_scene: PackedScene # Перетащи CorridorV сюда
 
-const ROOM_SIZE_X = 848
-const ROOM_SIZE_Y = 672
-const CORRIDOR_LENGTH = 64
-# Размер сетки карты 
+# Размер комнаты и КОРидОРА в пикселях
+const ROOM_SIZE_X = 864 # Твой размер
+const ROOM_SIZE_Y = 608 # Твой размер
+const CORRIDOR_LENGTH = 64 # ВНИМАНИЕ: должно делиться на размер тайла!
+
 const GRID_SIZE = 9
-
-
 enum RoomType { EMPTY, START, NORMAL, BOSS, TREASURE }
-
-# 2D массив скелета
 var layout = []
 
 func _ready():
 	generate_layout()
 	draw_map()
 
-
+# --- Генерация скелета (без изменений) ---
 func generate_layout():
-	# Инициализируем пустую карту
 	layout = []
 	for x in GRID_SIZE:
 		layout.append([])
 		for y in GRID_SIZE:
 			layout[x].append(RoomType.EMPTY)
 
-	# Старт (в центре)
 	var start_pos = Vector2i(GRID_SIZE / 2, GRID_SIZE / 2)
 	layout[start_pos.x][start_pos.y] = RoomType.START
 
-	# Босс (случайная позиция на правом краю)
 	var boss_pos = Vector2i(GRID_SIZE - 2, randi_range(1, GRID_SIZE - 2))
 	layout[boss_pos.x][boss_pos.y] = RoomType.BOSS
 
-	# Рисуем путь от старта к боссу
 	var current = start_pos
 	while current != boss_pos:
 		if randf() < 0.7 || current.y == boss_pos.y:
 			current.x += 1 if boss_pos.x > current.x else -1
 		else:
 			current.y += 1 if boss_pos.y > current.y else -1
-			
 		if layout[current.x][current.y] == RoomType.EMPTY:
 			layout[current.x][current.y] = RoomType.NORMAL
 
-	# Делаем случайные ответвления от пути
 	for _i in range(randi_range(3, 6)):
 		var rand_room = get_random_room_of_type(RoomType.NORMAL)
 		if rand_room == Vector2i.ZERO: break
-		
 		var dir = [Vector2i(1,0), Vector2i(-1,0), Vector2i(0,1), Vector2i(0,-1)].pick_random()
 		var new_pos = rand_room + dir
-		
 		if is_valid_pos(new_pos) and layout[new_pos.x][new_pos.y] == RoomType.EMPTY:
 			layout[new_pos.x][new_pos.y] = RoomType.NORMAL
 
-# Вспомогательные функции
 func is_valid_pos(pos):
 	return pos.x >= 0 and pos.x < GRID_SIZE and pos.y >= 0 and pos.y < GRID_SIZE
 
@@ -71,9 +60,7 @@ func get_random_room_of_type(type):
 	if valid_rooms.is_empty(): return Vector2i.ZERO
 	return valid_rooms.pick_random()
 
-
-#ОТРИСОВКА КОМНАТ В МИРЕ
-
+# --- НОВАЯ ОТРИСОВКА С КОРИДОРАМИ ---
 func draw_map():
 	# Считаем общую ширину ячейки (Комната + Коридор)
 	var cell_size_x = ROOM_SIZE_X + CORRIDOR_LENGTH
@@ -120,22 +107,13 @@ func draw_map():
 					corr.position.x = room_pos.x + (ROOM_SIZE_X / 2) - (CORRIDOR_LENGTH / 2)
 					corr.position.y = room_pos.y + ROOM_SIZE_Y
 					add_child(corr)
-				
-				# Если это стартовая комната, можно сразу спавнить туда игрока
-				if layout[x][y] == RoomType.START:
-					spawn_player(room_instance)
+
 func check_neighbor(nx, ny):
 	if is_valid_pos(Vector2i(nx, ny)):
 		return layout[nx][ny] != RoomType.EMPTY
 	return false
+
 func spawn_player(room):
-	# Ищем внутри комнаты узел с именем PlayerSpawn
 	var spawn_marker = room.find_child("PlayerSpawn", true, false)
 	if spawn_marker:
-		
-		# var player = preload("res://player.tscn").instantiate()
-		# player.global_position = spawn_marker.global_position
-		# add_child(player)
 		print("Игрок здесь: ", spawn_marker.global_position)
-	else:
-		print("В комнате нет узла PlayerSpawn!")
