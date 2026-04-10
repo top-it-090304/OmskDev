4d25ca40392ce9851f3c2b7b5ab3f1a71f145923
Nikita <n.shmykov@yandex.ru>
2026-04-10 11:08:54 +0600

    исправлены ошибки на портированной версии

--- World/UI/panel.gd
+++ World/UI/panel.gd
@@ -1,7 +1,7 @@
 extends Button
 
 @onready var timer = $Timer
-
+@export var target_scene:PackedScene
 func _on_pressed() -> void:
 	# 1. Отключаем кнопку, чтобы не было двойного нажатия при лаге
 	set_deferred("disabled", true)
@@ -10,8 +10,8 @@ func _on_pressed() -> void:
 	await get_tree().process_frame
 	
 	# 3. Переходим в меню
-	get_tree().change_scene_to_file("res://world/UI/menu.tscn")
+	get_tree().change_scene_to_packed(target_scene)
 
 func _on_timer_timeout() -> void:
 	# Вызываем ту же логику
-	get_tree().change_scene_to_file("res://world/UI/menu.tscn")
+	get_tree().change_scene_to_packed(target_scene)
